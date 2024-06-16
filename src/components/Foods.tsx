5fcd4c8b47e5615f0e42cf9bd249f0631831632a
armen <armenbakir@hotmail.com>
2024-06-16 13:05:09 +0200

    paginated foods

--- src/components/Foods.tsx
+++ src/components/Foods.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import { getFoods } from "../services/fakeFoodService";
 import Favorite from "./Favorite";
-import Welcome from "./Welcome";
 import Pagination from "./Pagination";
+import ListGroup from "./ListGroup";
+import { Category, getCategories } from "../services/fakeCategoryService";
+import { paginate } from "../utils";
 
+const DEFAULT_CATEGORY: Category = { _id: "", name: "All categories" };
+const PAGE_SIZE = 4;
 function Foods() {
   const [foods, setFoods] = useState(getFoods());
   const [selectedPage, setSelectedPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState(DEFAULT_CATEGORY);
 
   function handleDelete(id: string) {
     const newFoods = foods.filter((food) => food._id !== id);
@@ -25,52 +30,62 @@ function Foods() {
 
   if (foods.length === 0) return <p>There are no foods in the database</p>;
 
+  const paginatedFoods = paginate(foods, PAGE_SIZE, selectedPage);
+
   return (
-    <div className="container">
-      <Welcome />
-      <p className="m-2">Showing {foods.length} foods in the database</p>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Category</th>
-            <th scope="col">Price</th>
-            <th scope="col">Stock</th>
-            <th />
-            <th />
-          </tr>
-        </thead>
-        <tbody>
-          {foods.map((food) => (
-            <tr key={food._id}>
-              <td>{food.name}</td>
-              <td>{food.category.name}</td>
-              <td>{food.price}</td>
-              <td>{food.numberInStock}</td>
-              <td>
-                <Favorite
-                  isFavored={Boolean(food.isFavored)}
-                  onFavor={() => handleFavor(food._id)}
-                />
-              </td>
-              <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(food._id)}
-                >
-                  Delete
-                </button>
-              </td>
+    <div className="row container pt-3">
+      <div className="col-3">
+        <ListGroup
+          items={[DEFAULT_CATEGORY, ...getCategories()]}
+          selectedItem={selectedCategory}
+          onItemSelect={setSelectedCategory}
+        />
+      </div>
+      <div className="col">
+        <p className="m-2">Showing {foods.length} foods in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Category</th>
+              <th scope="col">Price</th>
+              <th scope="col">Stock</th>
+              <th />
+              <th />
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <Pagination
-        totalCount={foods.length}
-        pageSize={4}
-        selectedPage={selectedPage}
-        onPageSelect={setSelectedPage}
-      />
+          </thead>
+          <tbody>
+            {paginatedFoods.map((food) => (
+              <tr key={food._id}>
+                <td>{food.name}</td>
+                <td>{food.category.name}</td>
+                <td>{food.price}</td>
+                <td>{food.numberInStock}</td>
+                <td>
+                  <Favorite
+                    isFavored={Boolean(food.isFavored)}
+                    onFavor={() => handleFavor(food._id)}
+                  />
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(food._id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          totalCount={foods.length}
+          pageSize={PAGE_SIZE}
+          selectedPage={selectedPage}
+          onPageSelect={setSelectedPage}
+        />
+      </div>
     </div>
   );
 }
