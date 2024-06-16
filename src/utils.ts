9123cfc293093403fdd6f049c00d64c5827a47b9
armen <armenbakir@hotmail.com>
2024-06-16 13:04:51 +0200

    pagination function

--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,14 @@ export function range(startNumber: number, endNumber: number): number[] {
   }
   return array;
 }
+
+export function paginate<T>(
+  items: T[],
+  pageSize: number,
+  selectedPage: number
+) {
+  const startIndex = pageSize * (selectedPage - 1);
+  const endIndex = pageSize * selectedPage;
+
+  return items.slice(startIndex, endIndex);
+}
