be9111575186563bd3447b3d607a34f40319e64b
armen <armenbakir@hotmail.com>
2024-06-18 12:28:06 +0200

    added navbar with links

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { FoodsPage } from "@pages";
+import NavBar from "@components/NavBar";
 import "./App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <FoodsPage />;
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,17 @@
 import App from "@App";
-import { CustomersPage, NotFoundPage, OrdersPage } from "@pages";
+import { CustomersPage, FoodsPage, NotFoundPage, OrdersPage } from "@pages";
 import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App />, errorElement: <NotFoundPage /> },
-  { path: "/customers", element: <CustomersPage /> },
-  { path: "/orders", element: <OrdersPage /> },
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/foods", element: <FoodsPage /> },
+      { path: "/customers", element: <CustomersPage /> },
+      { path: "/orders", element: <OrdersPage /> },
+    ],
+  },
 ]);
 
 export default router;
